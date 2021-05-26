396d5acd91b23d112d3c3c27ba79eaf2866c2789
Thanh <thanhpv.tvh@vnpt.vn>
2021-05-26 13:47:58 +0700

    PVT - Upgrade to do

--- app/Modules/ToDo/Views/danh-sach-to-do.blade.php
+++ app/Modules/ToDo/Views/danh-sach-to-do.blade.php
@@ -10,32 +10,27 @@
                     <ul id="items-list" class="moveable flex-column-reverse todo-list">
                         @foreach($toDos as $index => $toDo)     
                             @php
-                                $ngay_tao = 'Ngày tạo: '.date('d/m/Y H:i:s',strtotime($toDo['ngay_tao']));
-                                $han_xu_ly='';
-                                if ($toDo['han_xu_ly']) {
-                                    $han_xu_ly = ' - Hạn xử lý: '.date('d/m/Y H:i:s',strtotime($toDo['han_xu_ly']));
-                                }
-                                $text=$toDo['noi_dung'].'<br><samll class="text-muted">'.$ngay_tao.$han_xu_ly.'</small>';
+                                $ngay_tao = date('d/m/Y H:i:s',strtotime($toDo['ngay_tao']));
+                                $han_xu_ly = date('d/m/Y H:i:s',strtotime($toDo['han_xu_ly']));
+                                $text=$toDo['noi_dung'].'<br>'.$ngay_tao;
                             @endphp           
-                            <li draggable="true" @if($toDo['ngay_hoan_thanh']!='') class="draggable completed" @else class="draggable" @endif>
-                                <div class="form-check">
+                            <li draggable="true" @if($toDo['ngay_hoan_thanh']!='') class="draggable completed" @else class="draggable" @endif style="display: block !important;">
+                                <div class="form-check" style="display: flex">
                                     <label class="form-check-label">
                                         <input class="checkbox" type="checkbox" data-id="{{$toDo['id']}}" @if($toDo['ngay_hoan_thanh']!='') checked="checked" @endif>
-                                        @php
-                                            echo $text;
-                                        @endphp
+                                        {{$toDo['noi_dung']}}
                                     </label>               
-                                </div>        
-                                {{-- <div style="padding-left: 20px; text-align: right;">
-                                    <small class="text-muted">Ngày tạo: {{$ngay_tao}}<br>HXL: {{$han_xu_ly}}</small>
-                                </div> --}}
+                                      
+                                
                                 <!-- <i class="remove mdi mdi-close-circle-outline"></i> -->
                                 <i class="remove"></i>
                                 <p class="mb-0 font-weight-normal float-right text-primary btn-sua" data="{{$toDo['id']}}"><b style="padding-right: 5px"><i class="fa fa-wrench"></i></b>
                                 </p>
                                 <p class="mb-0 font-weight-normal float-right text-danger btn-xoa" data="{{$toDo['id']}}"><b><i class="fa fa-times-circle-o"></i></b>
                                 </p>
-                            </li>
+                            </div>
+                                      <small class="text-muted" style="padding-left: 30px"><b>Ngày tạo:</b> {{$ngay_tao}}. <b>HXL:</b> {{$han_xu_ly}}</small>            
+                            </li>                        
                         @endforeach             
                     </ul>
                 </div>
@@ -123,64 +118,64 @@
             }
         });
     });
-	var dragSrcEl = null;
+    var dragSrcEl = null;
 
-	function handleDragStart(e) {
-	  // Target (this) element is the source node.
-	  dragSrcEl = this;
+    function handleDragStart(e) {
+      // Target (this) element is the source node.
+      dragSrcEl = this;
 
-	  e.dataTransfer.effectAllowed = 'move';
-	  e.dataTransfer.setData('text/html', this.outerHTML);
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.outerHTML);
 
-	  this.classList.add('dragElem');
-	}
-	function handleDragOver(e) {
-	  if (e.preventDefault) {
-	    e.preventDefault(); // Necessary. Allows us to drop.
-	  }
-	  this.classList.add('over');
+      this.classList.add('dragElem');
+    }
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault(); // Necessary. Allows us to drop.
+      }
+      this.classList.add('over');
 
-	  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+      e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
 
 
-	  return false;
-	}
+      return false;
+    }
 
-	function handleDragEnter(e) {
-	    e.preventDefault();
+    function handleDragEnter(e) {
+        e.preventDefault();
 
-	  // this / e.target is the current hover target.
-	}
+      // this / e.target is the current hover target.
+    }
 
-	function handleDragLeave(e) {
-	  this.classList.remove('over');  // this / e.target is previous target element.
-	}
+    function handleDragLeave(e) {
+      this.classList.remove('over');  // this / e.target is previous target element.
+    }
 
-	function handleDrop(e) {
-	  // this/e.target is current target element.
-	  if (e.stopPropagation) {
-	    e.stopPropagation(); // Stops some browsers from redirecting.
-	  }
+    function handleDrop(e) {
+      // this/e.target is current target element.
+      if (e.stopPropagation) {
+        e.stopPropagation(); // Stops some browsers from redirecting.
+      }
 
-	  // Don't do anything if dropping the same column we're dragging.
-	  if (dragSrcEl != this) {
-	    // Set the source column's HTML to the HTML of the column we dropped on.
-	    //alert(this.outerHTML);
-	    //dragSrcEl.innerHTML = this.innerHTML;
-	    //this.innerHTML = e.dataTransfer.getData('text/html');
-	    dragSrcEl.parentNode.removeChild(dragSrcEl);
-	    var dropHTML = e.dataTransfer.getData('text/html');
-	    this.insertAdjacentHTML('beforebegin',dropHTML);
-	    var dropElem = this.previousSibling;
-	    addDnDHandlers(dropElem);
+      // Don't do anything if dropping the same column we're dragging.
+      if (dragSrcEl != this) {
+        // Set the source column's HTML to the HTML of the column we dropped on.
+        //alert(this.outerHTML);
+        //dragSrcEl.innerHTML = this.innerHTML;
+        //this.innerHTML = e.dataTransfer.getData('text/html');
+        dragSrcEl.parentNode.removeChild(dragSrcEl);
+        var dropHTML = e.dataTransfer.getData('text/html');
+        this.insertAdjacentHTML('beforebegin',dropHTML);
+        var dropElem = this.previousSibling;
+        addDnDHandlers(dropElem);
 
-	  }
-	  this.classList.remove('over');
-	  return false;
-	}
+      }
+      this.classList.remove('over');
+      return false;
+    }
 
-	function handleDragEnd(e) {
-	   // this/e.target is the source node.
+    function handleDragEnd(e) {
+       // this/e.target is the source node.
         
         var dsId='';
         jQuery('.checkbox').each(function( index ) {
@@ -195,25 +190,25 @@
 
         // Post danh sách id chỗ này xài hàm này, 2 trường cuối để rổng ''
         //postId(_token, id, url, urlRefreshData, classNameRefreshData);
-	   this.classList.remove('over');
+       this.classList.remove('over');
 
-	  /*[].forEach.call(cols, function (col) {
-	    col.classList.remove('over');
-	  });*/
-	}
+      /*[].forEach.call(cols, function (col) {
+        col.classList.remove('over');
+      });*/
+    }
 
-	function addDnDHandlers(elem) {
-	  elem.addEventListener('dragstart', handleDragStart, false);
-	  elem.addEventListener('dragenter', handleDragEnter, false)
-	  elem.addEventListener('dragover', handleDragOver, false);
-	  elem.addEventListener('dragleave', handleDragLeave, false);
-	  elem.addEventListener('drop', handleDrop, false);
-	  elem.addEventListener('dragend', handleDragEnd, false);
+    function addDnDHandlers(elem) {
+      elem.addEventListener('dragstart', handleDragStart, false);
+      elem.addEventListener('dragenter', handleDragEnter, false)
+      elem.addEventListener('dragover', handleDragOver, false);
+      elem.addEventListener('dragleave', handleDragLeave, false);
+      elem.addEventListener('drop', handleDrop, false);
+      elem.addEventListener('dragend', handleDragEnd, false);
 
-	}
+    }
 
-	var cols = document.querySelectorAll('#items-list .draggable');
-	[].forEach.call(cols, addDnDHandlers);
+    var cols = document.querySelectorAll('#items-list .draggable');
+    [].forEach.call(cols, addDnDHandlers);
 
 </script>
 
