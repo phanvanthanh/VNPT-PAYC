d89c65f7784a3f9ceed2109269a785e51e5fd116
Thanh <thanhpv.tvh@vnpt.vn>
2021-07-08 10:47:21 +0700

    PVT

--- app/Modules/TrangChu/Views/home.blade.php
+++ app/Modules/TrangChu/Views/home.blade.php
@@ -12,7 +12,7 @@
                      <div class="error-mode float-right"></div> 
                   </div>
                </div>
-               <div class="row">
+               {{-- <div class="row">
 	               	<div class="col-sm-12">
 	               		<form class="forms-sample frm-them-moi" id="frm-them-moi" name="frm-them-moi" method="POST" action="{{ route('sso-dang-nhap-2') }}">
 										  {{ csrf_field() }}
@@ -25,7 +25,7 @@
 										  <button type="submit" class="btn btn-vnpt btn-them-moi"><i class="icon-check"></i>Đăng nhập SSO 2</button>
 										</form>
 	               	</div>
-               </div>
+               </div> --}}
 	        </div>
 	    </div>
     </div>
