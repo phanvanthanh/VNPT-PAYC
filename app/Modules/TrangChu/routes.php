53ac3ed15467dfd1520cf2e0afa366713ed7a84a
Thanh <thanhpv.tvh@vnpt.vn>
2021-07-09 22:38:24 +0700

    PVT

--- app/Modules/TrangChu/routes.php
+++ app/Modules/TrangChu/routes.php
@@ -10,10 +10,30 @@ Route::group(
             'uses' => 'TrangChuController@home'
         ]);
 
-        Route::get('/admin/clear-all', function() {
+        Route::get('clear-all', function() {
             Artisan::call('view:clear');
             Artisan::call('cache:clear');
             Artisan::call('config:clear');
+            Artisan::call('route:clear');
+            return redirect()->route('to-do');
+        });
+
+        Route::get('clear-cache', function() {
+            Artisan::call('cache:clear');
+            return redirect()->route('to-do');
+        });
+
+        Route::get('clear-view', function() {
+            Artisan::call('view:clear');
+            return redirect()->route('to-do');
+        });
+
+        Route::get('clear-config', function() {
+            Artisan::call('config:clear');
+            return redirect()->route('to-do');
+        });
+
+        Route::get('clear-route', function() {
             Artisan::call('route:clear');
             return redirect()->route('to-do');
         });
