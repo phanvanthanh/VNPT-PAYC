4c7d78619fb30a51e63a8d7e4d10944ace1574fb
Thanh <thanhpv.tvh@vnpt.vn>
2021-07-09 17:02:19 +0700

    PVT

--- app/Modules/TrangChu/routes.php
+++ app/Modules/TrangChu/routes.php
@@ -12,7 +12,9 @@ Route::group(
 
         Route::get('/clear-cache', function() {
             $exitCode = Artisan::call('cache:clear');
+            Artisan::call('view:clear');
             // return what you want
+            
             return redirect()->route('to-do');
         });
 
