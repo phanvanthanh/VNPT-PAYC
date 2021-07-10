24409953b1efe586179e4a7bc3e54be70fc611f7
Thanh <thanhpv.tvh@vnpt.vn>
2021-07-10 10:50:08 +0700

    PVT fix

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,57 +6,57 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="csrf-token" content="{{ csrf_token() }}">
       <title>Báo cáo tuần - @yield('title')</title>
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-tags-input/jquery.tagsinput.min.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/fontawesome-stars.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bars-1to10.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bars-horizontal.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bars-movie.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bars-pill.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bars-reversed.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bars-square.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/bootstrap-stars.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/css-stars.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-bar-rating/fontawesome-stars-o.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/font-awesome/css/font-awesome.min.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/dropify/dropify.min.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-file-upload/uploadfile.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/clockpicker/jquery-clockpicker.min.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-asColorPicker/css/asColorPicker.min.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
-     <link rel="stylesheet" href="{{ secure_asset('public/vendors/x-editable/bootstrap-editable.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/jquery-tags-input/jquery.tagsinput.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/fontawesome-stars.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bars-1to10.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bars-horizontal.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bars-movie.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bars-pill.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bars-reversed.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bars-square.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/bootstrap-stars.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/css-stars.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-bar-rating/fontawesome-stars-o.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/font-awesome/css/font-awesome.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/dropify/dropify.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-file-upload/uploadfile.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/clockpicker/jquery-clockpicker.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/jquery-asColorPicker/css/asColorPicker.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('public/vendors/x-editable/bootstrap-editable.css') }}">
 
 
 
       <!-- Sumery plugin -->
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/summernote/dist/summernote-bs4.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/quill/quill.snow.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/simplemde/simplemde.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/summernote/dist/summernote-bs4.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/quill/quill.snow.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/simplemde/simplemde.min.css') }}">
 
 
       <!-- plugins:css -->
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/mdi/css/materialdesignicons.min.css') }}" >
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/simple-line-icons/css/simple-line-icons.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/flag-icon-css/css/flag-icon.min.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/css/vendor.bundle.base.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/mdi/css/materialdesignicons.min.css') }}" >
+      <link rel="stylesheet" href="{{ asset('public/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/css/vendor.bundle.base.css') }}">
       <!-- endinject -->
       <!-- plugin css for this page -->
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/lightgallery/css/lightgallery.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/lightgallery/css/lightgallery.min.css') }}">
       <!-- End plugin css for this page -->
       <!-- Datatable -->
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
       <!-- End Datatable -->
       <!-- Font Asomware -->
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/font-awesome/css/font-awesome.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/font-awesome/css/font-awesome.min.css') }}">
       <!-- End font Asomeware -->
       <!-- inject:css -->
-      <link rel="stylesheet" href="{{ secure_asset('public/css/style.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/css/tree.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/css/loading-style.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/css/tree.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/css/loading-style.css') }}">
       <!-- endinject -->
-      <link rel="shortcut icon" href="{{ secure_asset('public/images/favicon.ico') }}">
+      <link rel="shortcut icon" href="{{ asset('public/images/favicon.ico') }}">
 
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/dragula/dragula.min.css') }}">
-      <link rel="stylesheet" href="{{ secure_asset('public/vendors/jquery-toast-plugin/jquery.toast.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/dragula/dragula.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('public/vendors/jquery-toast-plugin/jquery.toast.min.css') }}">
 
       <style type="text/css">
         .image-tile:hover{
